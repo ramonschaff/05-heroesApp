f8cf2222ce0144f1a6552c5945351fdbc4e550dd
schafframon <gabrieleks610923@gmail.com>
2022-09-11 20:08:49 -0400

    first commit

---
+++ src/app/heroes/pages/heroes-home/heroes-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-home',
+  templateUrl: './heroes-home.component.html',
+  styleUrls: ['./heroes-home.component.css']
+})
+export class HeroesHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
