a51f457a53989257002b16d16145b07e581eb020
schafframon <gabrieleks610923@gmail.com>
2022-09-15 18:59:27 -0400

    9/15

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-listado',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private HeroesService:HeroesService) { }
 
   ngOnInit(): void {
+
+    this.HeroesService.getHeroes()
+      .subscribe(resp => console.log(resp));
+     
+
   }
 
 }
