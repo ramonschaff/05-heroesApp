c0753e96d741035a75403756a04d3470d8a0b39d
ramonschaff <103288656+ramonschaff@users.noreply.github.com>
2022-09-20 16:58:06 -0400

    9/20 tarde

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-buscar',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino:string = '';
+  heroes:Heroe[]=[];
+  heroeSeleccionado!:Heroe | undefined;
+
+  constructor(private heroesService:HeroesService) { }
 
   ngOnInit(): void {
   }
+
+  buscando(){
+     this.heroesService.getSugerencias(this.termino)
+         .subscribe(heroes =>this.heroes=heroes)
+  }
+
+  opcionSeleccionada(event:MatAutocompleteSelectedEvent){
+ 
+      if (!event.option.value) {
+        this.heroeSeleccionado==undefined;
+        return;
+        
+      }
+
+    const heroe : Heroe = event.option.value;
+    this.termino= heroe.superhero
+    console.log(this.termino);
+
+    this.heroesService.getHeroesId(heroe.id)
+    .subscribe(heroe=>this.heroeSeleccionado=heroe);
+
+    }
 
 }
